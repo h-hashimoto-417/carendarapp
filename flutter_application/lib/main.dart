d531120422dabf41dcaedd4cbfda3c775dbd9605
akequreru <yu.tasaki.417@gmail.com>
2025-06-25 18:14:14 +0900

    pagechange

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
       ),
       debugShowCheckedModeBanner: false,
-      home: ScreenCalendar(),
+      home: ScreenHomeToday(today: DateTime.now()),
       );
   }
 }
