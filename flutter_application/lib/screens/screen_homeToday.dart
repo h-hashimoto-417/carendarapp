54581ec09d314295e12e441142c60d27b63b67c6
h-hashimoto-417 <hitomi.seyomo484@outlook.com>
2025-06-25 15:18:15 +0900

    ブロック表示

--- flutter_application/lib/screens/screen_homeToday.dart
+++ flutter_application/lib/screens/screen_homeToday.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 //import 'package:flutter_application/data/TaskManager.dart';
 import 'package:flutter_application/data/database.dart';
 
-
-
-
 class ScreenHomeToday extends StatefulWidget {
   const ScreenHomeToday({super.key, required this.today});
   final DateTime today;
@@ -13,39 +10,33 @@ class ScreenHomeToday extends StatefulWidget {
   State<ScreenHomeToday> createState() => _ScreenHomeTodayState();
 }
 
-
 class _ScreenHomeTodayState extends State<ScreenHomeToday> {
-  
-  //final DateTime today = widget.today; 
+  //final DateTime today = widget.today;
   int countFromToday = 0;
-  int  month = 0;
-  int  day = 0;
-  int  weekday = 0;
+  int month = 0;
+  int day = 0;
+  int weekday = 0;
   int count = 0;
   //DateTime someday = DateTime.now();
-  
+
   @override
   void initState() {
     super.initState();
     DateTime now = widget.today;
     month = now.month;
     day = now.day;
-    weekday = now.weekday;  
-    
+    weekday = now.weekday;
   }
 
-
-  //@override 
+  //@override
   void _dateTransition() {
     setState(() {
-      
-      DateTime someday;// = widget.today.add(Duration(days: countFromToday));
-      if(countFromToday == 0) {
-        someday = widget.today;    
-      }else if(countFromToday > 0) {
+      DateTime someday; // = widget.today.add(Duration(days: countFromToday));
+      if (countFromToday == 0) {
+        someday = widget.today;
+      } else if (countFromToday > 0) {
         someday = widget.today.add(Duration(days: countFromToday));
-      }
-      else {
+      } else {
         someday = widget.today.subtract(Duration(days: (-countFromToday)));
       }
 
@@ -53,14 +44,11 @@ class _ScreenHomeTodayState extends State<ScreenHomeToday> {
       day = someday.day;
       weekday = someday.weekday;
       count = countFromToday;
-      
-    
     });
   }
-  
+
   @override
   Widget build(BuildContext context) {
-
     // DateTime now = widget.today;
     // month = now.month;
     // day = now.day;
@@ -72,26 +60,27 @@ class _ScreenHomeTodayState extends State<ScreenHomeToday> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         leading: IconButton(
           icon: const Icon(Icons.calendar_month, size: 45), // 左端のアイコン
-          onPressed: () => {      // カレンダーアイコンの動作を定義
-            Navigator.pushNamed(context, "/carendarPage")
-          },
+          onPressed:
+              () => {
+                // カレンダーアイコンの動作を定義
+                Navigator.pushNamed(context, "/carendarPage"),
+              },
         ),
         title: Text(
-          '$month月',  // 日付データを取得する！
+          '$month月', // 日付データを取得する！
           style: TextStyle(color: Colors.black),
         ),
         centerTitle: true,
         actions: [
-           IconButton(
-             icon: const Icon(Icons.edit_note, size: 45), // 右端のアイコン
-             onPressed: () {
+          IconButton(
+            icon: const Icon(Icons.edit_note, size: 45), // 右端のアイコン
+            onPressed: () {
               // 編集アイコンの動作を定義
-             },
-            ),
+            },
+          ),
         ],
       ),
 
-      
       // スクロールウィンドウの表示
       body: Stack(
         clipBehavior: Clip.none,
@@ -107,17 +96,43 @@ class _ScreenHomeTodayState extends State<ScreenHomeToday> {
                     decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(5),
-                      border: Border.all(color:Colors.black, width: 1),
+                      border: Border.all(color: Colors.black, width: 1),
                       //boxShadow: [BoxShadow(color: Colors.black, blurRadius: 1)],
                     ),
                     child: SingleChildScrollView(
                       child: Column(
-                        children: List.generate(25, (index) => Padding(
-                          padding: EdgeInsets.all(25),
-                          child: Align( 
-                            alignment: Alignment.topLeft,
-                            child: Text('$index : 00',),
-                        ))),
+                        children: List.generate(
+                          24,
+                          (index) => Container(
+                            padding: EdgeInsets.all(10),
+                            child: Column(
+                              crossAxisAlignment:
+                                  CrossAxisAlignment.start, // 左寄せ
+                              children: [
+                                Text(
+                                  '$index : 00',
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                                SizedBox(height: 6), // テキストと長方形の間にスペース
+                                Container(
+                                  width: double.infinity, // 横幅いっぱい
+                                  height: 60, // 高さ100の長方形
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(7),                                    
+                                  ),
+                                  //color: Colors.blue, // 色を指定
+                                ),
+                              ],
+                            ),
+                          ),
+
+                          //   padding: EdgeInsets.all(25),
+                          //   child: Align(
+                          //     alignment: Alignment.topLeft,
+                          //     child: Text('$index : 00',),
+                          // ))
+                        ),
                       ),
                     ),
                   ),
@@ -135,10 +150,13 @@ class _ScreenHomeTodayState extends State<ScreenHomeToday> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 IconButton(
-                  icon: Icon(Icons.arrow_left, size: 70, color: Colors.amberAccent),
+                  icon: Icon(
+                    Icons.arrow_left,
+                    size: 70,
+                    color: Colors.amberAccent,
+                  ),
                   onPressed: () {
                     countFromToday--;
-
                     _dateTransition();
                   },
                 ),
@@ -154,19 +172,28 @@ class _ScreenHomeTodayState extends State<ScreenHomeToday> {
                       children: [
                         Text(
                           weekdayName[weekday], // 日付データ(曜日)を取得する！
-                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                         Text(
-                          '$day',   // 日付けを表示
-                          style: TextStyle(fontSize: 43, fontWeight: FontWeight.bold),
+                          '$day', // 日付けを表示
+                          style: TextStyle(
+                            fontSize: 43,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-
                       ],
                     ),
                   ),
                 ),
                 IconButton(
-                  icon: Icon(Icons.arrow_right, size: 70, color: Colors.amberAccent),
+                  icon: Icon(
+                    Icons.arrow_right,
+                    size: 70,
+                    color: Colors.amberAccent,
+                  ),
                   onPressed: () {
                     countFromToday++;
                     //someday = now.add(Duration(days: countFromToday));
@@ -177,10 +204,8 @@ class _ScreenHomeTodayState extends State<ScreenHomeToday> {
             ),
           ),
         ],
-      )
-
+      ),
     );
-
   }
 }
 
@@ -189,7 +214,15 @@ class HalfMoonClipper extends CustomClipper<Path> {
   Path getClip(Size size) {
     Path path = Path();
     path.moveTo(0, 0); // 下部の左端
-    path.arcTo(Rect.fromCircle(center: Offset(size.width / 2, 0), radius: size.width / 2), 0, 3.14, false); // 半円を上向きに描画
+    path.arcTo(
+      Rect.fromCircle(
+        center: Offset(size.width / 2, 0),
+        radius: size.width / 2,
+      ),
+      0,
+      3.14,
+      false,
+    ); // 半円を上向きに描画
     path.lineTo(size.width, 0); // 下部の右端へ
     path.close();
     return path;
