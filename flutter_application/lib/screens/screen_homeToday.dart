ab3f684198eb1951a5be6e5fc60e5fc54d7aa4a2
h-hashimoto-417 <hitomi.seyomo484@outlook.com>
2025-07-08 15:07:34 +0900

    タスク数表示（完）

--- flutter_application/lib/screens/screen_homeToday.dart
+++ flutter_application/lib/screens/screen_homeToday.dart
@@ -705,7 +705,7 @@ class _ScreenHomeTodayState extends ConsumerState<ScreenHomeToday> {
                                                       taskHourMap.containsKey(
                                                         index,
                                                       )) {
-                                                    setNumTempoPlacedTask(taskIDs[index]);                                                   
+                                                    setNumTempoPlacedTask(taskHourMap[index]!.id);                                                   
                                                     setState(() {
                                                       taskHourMap.remove(index);
                                                       selectedHour = null;
